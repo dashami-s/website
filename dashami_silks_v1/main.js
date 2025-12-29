668b1e486b7817bdc3814d72560c2d6fb2f5a7fc
Aditya <adisharma102000@gmail.com>
2025-12-29 15:37:44 +0530

    Update main.js

--- dashami_silks_v1/main.js
+++ dashami_silks_v1/main.js
@@ -312,7 +312,7 @@ function createProductCard(p) {
         }
     }
     
-    const productUrl = `${window.location.origin}/product.html?id=${p.id}`;
+    const productUrl = `${window.location.origin}/website/dashami_silks_v1/product.html?id=${p.id}`;
     const msg = `Hello Dashami Silk, I am interested in:\n*${safeName}*\nID: ${p.id}\nLink: ${productUrl}`;
     const rawWaLink = `whatsapp://send?phone=${MY_NUMBER}&text=${encodeURIComponent(msg)}`;
     const link = `social_redirect.html?target=${encodeURIComponent(rawWaLink)}&platform=WhatsApp`;
